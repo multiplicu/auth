e96b3300cadc305f3116cec673c47f0c7aa5c7bc
Andy Rose <andyrose@hey.com>
2021-07-20 14:29:46 -0500

    style: changes after running style:fix

--- src/lib/auth.module.ts
+++ src/lib/auth.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
 	declarations: [LoginComponent],
 	imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
-	exports: [LoginComponent]
+	exports: [LoginComponent],
 })
 export class AuthModule {}
